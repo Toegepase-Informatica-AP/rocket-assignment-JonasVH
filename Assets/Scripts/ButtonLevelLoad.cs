e45db21627c765a8f704600cbca61833053ee949
JonasVH <46106472+JonasVH@users.noreply.github.com>
2020-10-12 10:47:27 +0200

    Rocket assignment Jonas Van Haute
    aanmaken project/gitignore

---
+++ Assets/Scripts/ButtonLevelLoad.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLevelLoad : MonoBehaviour
+{
+    public string mLevelToLoad;
+
+    public void LoadLevel() 
+    {
+        //Load the new level (mLevelLoad)
+        SceneManager.LoadScene(mLevelToLoad);
+    }
+
+}
---
+++ Assets/Scripts/Enemies.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    private Vector3 startPosition;
+    bool up = true;
+    // Use this for initialization
+    void Start()
+    {
+        //maxSpeed = 3;
+        startPosition = transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        MoveVertical();
+    }
+    void MoveVertical()
+    {
+        var temp = transform.position;
+        print(up);
+        if (up == true)
+        {
+            temp.y += 0.03f;
+            transform.position = temp;
+            if (transform.position.y >= 35f)
+            {
+                up = false;
+            }
+        }
+        if (up == false)
+        {
+            temp.y -= 0.03f;
+            transform.position = temp;
+            if (transform.position.y <= 15f)
+            {
+                up = true;
+            }
+        }
+    }
+}
